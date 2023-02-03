d5116b3cff451692e9663a5007563b7e8fc98436
hoscho <jgeist@gmx.net>
2023-02-03 12:13:37 +0100

    Initial commit

---
+++ src/main/java/de/jgsoftwares/dnsserver/dockerdnsserver/Dockerdnsserver.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package de.jgsoftwares.dnsserver.dockerdnsserver;
+
+/**
+ *
+ * @author root
+ */
+public class Dockerdnsserver {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
