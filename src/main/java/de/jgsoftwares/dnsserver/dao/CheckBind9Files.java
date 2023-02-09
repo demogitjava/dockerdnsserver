60f65f3874bbdb3297f7c8c3cc473ac8bbc54c33
hoscho <jgeist@gmx.net>
2023-02-09 12:06:02 +0100

    add dao for check bind9 zones files

---
+++ src/main/java/de/jgsoftwares/dnsserver/dao/CheckBind9Files.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package de.jgsoftwares.dnsserver.dao;
+
+/**
+ *
+ * @author root
+ */
+public class CheckBind9Files implements iCheckBind9Files
+{
+    
+}
---
+++ src/main/java/de/jgsoftwares/dnsserver/dao/iCheckBind9Files.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package de.jgsoftwares.dnsserver.dao;
+
+/**
+ *
+ * @author root
+ */
+public interface iCheckBind9Files {
+    
+}
