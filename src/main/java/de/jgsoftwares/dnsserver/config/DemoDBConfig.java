0461f9f141e14cf59dd9a561f48a2721c64113ac
hoscho <jgeist@gmx.net>
2023-02-13 04:30:24 +0100

    add Reverse Controller edit DemoDbConfig import settings

--- src/main/java/de/jgsoftwares/dnsserver/config/DemoDBConfig.java
+++ src/main/java/de/jgsoftwares/dnsserver/config/DemoDBConfig.java
@@ -8,26 +8,13 @@ package de.jgsoftwares.dnsserver.config;
 
 
 import java.sql.SQLException;
-import java.util.HashMap;
-
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
 
 import com.zaxxer.hikari.HikariConfig;
 import org.h2.tools.Server;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
@@ -42,11 +29,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 @EnableTransactionManagement
-
 @EnableJpaRepositories(basePackages = "de.jgsoftwares.dnsserver.dao",
         entityManagerFactoryRef = "entityManagerFactory",
         transactionManagerRef = "transactionManager")
---
+++ src/main/java/de/jgsoftwares/dnsserver/controller/CtrlDnsReverse.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package de.jgsoftwares.dnsserver.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CtrlDnsReverse implements iCtrlDnsReverse
+{
+    
+}
