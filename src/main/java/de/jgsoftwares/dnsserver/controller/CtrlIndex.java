b982fdb2c4aa7d716758d497279fc1114a507d4f
hoscho <jgeist@gmx.net>
2023-02-14 09:40:00 +0100

    add jquery to static folder
    MVC Spring Boot Controller is now
    loaded over the interface

--- src/main/java/de/jgsoftwares/dnsserver/controller/CtrlIndex.java
+++ src/main/java/de/jgsoftwares/dnsserver/controller/CtrlIndex.java
@@ -8,10 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import de.jgsoftwares.dnsserver.service.iSIndex;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
+
 public class CtrlIndex implements iCtrlIndex
 {
     @Autowired
--- src/main/java/de/jgsoftwares/dnsserver/controller/iCtrlIndex.java
+++ src/main/java/de/jgsoftwares/dnsserver/controller/iCtrlIndex.java
@@ -4,11 +4,17 @@
  */
 package de.jgsoftwares.dnsserver.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-
+@RequestMapping("/")
 public interface iCtrlIndex 
 {
  
+    
+    // load default landingpage
+    // de
+    @GetMapping({"/", "index"})
     public ModelAndView index();
 }
