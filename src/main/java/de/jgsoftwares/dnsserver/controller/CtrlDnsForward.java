0e00b0727e5f994720722c598dc14e56db510b88
hoscho <jgeist@gmx.net>
2023-02-23 11:59:22 +0100

    Edit Form post to response url of the MVC index controller

--- src/main/java/de/jgsoftwares/dnsserver/controller/CtrlDnsForward.java
+++ src/main/java/de/jgsoftwares/dnsserver/controller/CtrlDnsForward.java
@@ -2,8 +2,11 @@ package de.jgsoftwares.dnsserver.controller;
 
 import de.jgsoftwares.dnsserver.model.MDNS;
 import de.jgsoftwares.dnsserver.service.iSBindForward;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CtrlDnsForward implements iCtrlDnsForward
@@ -13,9 +16,10 @@ public class CtrlDnsForward implements iCtrlDnsForward
    @Autowired
    iSBindForward ibindforward;
   
+   @Autowired
+   iCtrlIndex ictlindex;
    
-   
-  
+   HttpServletRequest request;
     
    @Override
    public void addforwardrecord(MDNS mdns)
@@ -23,9 +27,24 @@ public class CtrlDnsForward implements iCtrlDnsForward
        ibindforward.getDaoentry().saveforward(mdns);
    }
    
+    /**
+     *
+     * @param request
+     * @param response
+     * @return index MVC Controller
+     */
     @Override
-    public void createFile()
+    public String createFile(HttpServletRequest request, HttpServletResponse response)
     {
         ibindforward.getDaoentry().createnamedconflocal();
+        String redirectUrl = request.getPathInfo();
+        if(redirectUrl == null)
+        {
+            redirectUrl = "index";
+        }
+        return "redirect:" + redirectUrl;
     }
+    
+    
+    
 }
